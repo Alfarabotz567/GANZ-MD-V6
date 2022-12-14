060dae630ca9a977d2bd2bfd287bc916f6e9f09e
Alfarabotz567 <109883756+Alfarabotz567@users.noreply.github.com>
2022-12-15 05:52:51 +0700

    Update hallo.js

--- plugins/hallo.js
+++ plugins/hallo.js
@@ -1,7 +1,7 @@
 let handler = async (m, { conn, usedPrefix: _p, __dirname, args }) => {
-let text = `*Hyy bruhhh*👋🏻\n\n*Saya SilenceBOT salken yaa!* 😆`
+let text = `*Hyy bruhhh*👋🏻\n\n*Saya Alfarabotz salken yaa!* 😆`
 const templateButtons = [
-    {index: 1, urlButton: {displayText: 'Chat Owner', url: 'https://wa.me/6289631008798'}},
+    {index: 1, urlButton: {displayText: 'Chat Owner', url: 'https://wa.me/6285624823115'}},
     {index: 2, quickReplyButton: {displayText: 'Menu', id: '.menu'}},
     {index: 3, quickReplyButton: {displayText: 'RPG', id: '.? rpg'}}
 ]
